22dcaf603c120ba1d2045dbffb8f08e083363684
truelanz <devalanoliveira@gmail.com>
2025-03-26 09:41:53 -0300

    remove no used imports

--- backend/src/main/java/com/truelanz/catalog/repositories/CategoryRepository.java
+++ backend/src/main/java/com/truelanz/catalog/repositories/CategoryRepository.java
@@ -3,9 +3,7 @@ package com.truelanz.catalog.repositories;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-import com.truelanz.catalog.dto.CategoryDTO;
 import com.truelanz.catalog.entities.Category;
-import java.util.List;
 
 
 @Repository
