8326e745883085ba61ffffd2912c54f5eb0ce27e
truelanz <devalanoliveira@gmail.com>
2025-04-24 12:54:41 -0300

    iframes h2 liberation on SecurityConfig class

--- backend/src/main/java/com/truelanz/catalog/config/SecurityConfig.java
+++ backend/src/main/java/com/truelanz/catalog/config/SecurityConfig.java
@@ -5,14 +5,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-//Classe para liberar acesso aos endpoints com o spring security habilitado no pom.xml. (temporário para testes)
 @Configuration
 public class SecurityConfig {
 
 	@Bean
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http.csrf(csrf -> csrf.disable());
+		http.csrf(csrf -> csrf.disable())
+        // LIBERA iframes para vizualização de h2-console no navegador.
+        .headers(headers -> headers.frameOptions(frame -> frame.disable())); 
+
 		http.authorizeHttpRequests(auth -> auth.anyRequest().permitAll());
 		return http.build();
 	}
 }
+
